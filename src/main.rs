629aefd40d072d65ec83bf8875838bcc0d64c743
Adam Corbo <43587108+hamburgerguy@users.noreply.github.com>
2022-12-12 16:59:52 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -52,14 +52,16 @@ impl ReverseProxy {
     }
 }
 
-
+/*
 #[get("/api/blocks/0")]
 fn proxy(info: State<ReverseProxy>) -> JsonValue {
     let response = reqwest::blocking::get(&info.origin).unwrap();
     json!([{        "id": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",        "height": 0,        "version": 1,        "timestamp": 1231006505,        "tx_count": 1,        "size": 285,        "weight": 816,        "merkle_root": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",        "previousblockhash": null,        "mediantime": 1231006505,        "nonce": 2083236893,        "bits": 486604799,        "difficulty": 1    }])
 }
+*/
 
 fn main() {
+    /*
     let info = ReverseProxy {
         cache: HashMap::new(),
         origin: "https://blockstream.info".to_string(),
@@ -69,4 +71,5 @@ fn main() {
         .manage(info)
         .mount("/", routes![proxy])
         .launch();
+*/
 }
